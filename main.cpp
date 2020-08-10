5473fce2661768a709f13643c839fba4bdb82002
Oleg <oleg.fomenko2002@gmail.com>
2020-08-10 16:13:23 +0300

    initial commit (with phi and gcd functions prototype)

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "main_algos/algebra.h"
+
+int main() {
+
+}
---
+++ main_algos/algebra.h
@@ -0,0 +1,16 @@
+#ifndef CODE_ALGEBRA_H
+#define CODE_ALGEBRA_H
+
+/**
+ * GCD Euclid's algorithm function
+ * @tparam T - values type
+ * @param a - first val
+ * @param b - second val
+ * @return greatest common divider
+ */
+template <class T> T gcd(T a, T b);
+
+template <class T> T phi(T n);
+
+
+#endif //CODE_ALGEBRA_H

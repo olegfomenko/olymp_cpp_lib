e4411878e28e3741eb531eef28daf6e1dd9838ee
Oleg <oleg.fomenko2002@gmail.com>
2020-08-10 17:33:21 +0300

    adding Sieve of Eratosthenes and Euler function

--- main_algos/algebra.h
+++ main_algos/algebra.h
@@ -1,6 +1,9 @@
 #ifndef CODE_ALGEBRA_H
 #define CODE_ALGEBRA_H
 
+#include "iostream"
+using namespace std;
+
 namespace alg {
     /**
      * GCD Euclid's algorithm function
@@ -24,7 +27,77 @@ namespace alg {
         return (a / gcd(a, b)) * b;
     }
 
-    template <class T> T phi(T n);
+    /**
+     * Sieve of Eratosthenes algorithm with complexity O( N * logN)
+     * Sets array with 0 - value on prime index and least prime divider value on non-prime indexes
+     * @tparam T - value type
+     * @param start - address of array start's
+     * @param end - address of array end's
+     */
+    template <typename T> void fill_prime(T * start, T * end) {
+        int N = end - start;
+        fill(start, end, 0);
+
+        for(T i = 2; i < N / i + 1; ++i) {
+            if(start[i] != 0) continue;
+
+            start[i] = i;
+
+            for(T j = i * i; j < N; j += i) start[j] = i;
+        }
+    }
+
+    /**
+     * Sieve of Eratosthenes algorithm with complexity O( N * logN)
+     * Sets array with 0 - value on prime index and least prime divider value on non-prime indexes
+     * @tparam T - value type
+     * @param start - address of array start's
+     * @param N - array size
+     */
+    template <typename T> void fill_prime(T * start, T N) {
+        fill_prime(start, start + N);
+    }
+
+    /**
+     * Euler function with complexity: O( logN )
+     * @tparam T - value type
+     * @param n
+     * @param array with 0 - value on prime index and least prime divider value on non-prime indexes
+     * @return count of natural values i from 1 to n that gcd(i, n) = 1
+     */
+    template <typename T> T phi(T n, T * prime) {
+        T result = n;
+
+        while(n > 1) {
+            result = (result / prime[n]) * (prime[n] - 1);
+            n /= prime[n];
+        }
+
+        return result;
+    }
+
+    /**
+     * Euler function with complexity: O( sqrt(N) )
+     * @tparam T - value type
+     * @param n
+     * @return count of natural values i from 1 to n that gcd(i, n) = 1
+     */
+    template <class T> T phi(T n) {
+        T result = n;
+
+        for(T i = 2; i < n / i + 1 && n > 1; ++i) {
+            if(n % i == 0) {
+                result = (result / i) * (i - 1);
+            }
+
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
+
+        if(n > 1) result = (result / n) * (n - 1);
+        return result;
+    }
 }
 
 
@@ -32,4 +105,5 @@ namespace alg {
 
 
 #endif //CODE_ALGEBRA_H
+
 
